f42406794e79c2c616ce006385f3bb1572be4ef1
Andrew <brokenhoax@outlook.com>
2020-11-16 01:32:05 -0600

    Added Drop-Down Menu for Navlink

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Navbar from "./components/navbar/Navbar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 import Blog from "./components/blog/Blog";
 import "./styles/app.css";
 
 function App() {
   return (
-    <div className="h-screen bg-gray-800 text-gray-400">
-      <Navbar />
-      <Blog />
-    </div>
+    <Router>
+      <div className="h-screen bg-gray-800 text-gray-400">
+        <Navbar />
+        <Blog />
+      </div>
+    </Router>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,13 @@
-import React, { useState, createContext } from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../../styles/app.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faMinus } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../assets/logo.png";
 
-export const NavbarContext = createContext();
-
-
-const Navbar = () => {
-
-  const [navvy, setNavvy] = useState(
-    {
-      isActive: false,
-    }
-  )
-  
-
+function Navbar() {
+  const [toggle, setNavvy] = useState(false);
 
   const navLinks = [
     {
@@ -36,30 +28,51 @@ const Navbar = () => {
     },
   ];
 
-    function setNavbarstatus() {
-      if (navvy.isActive) {
-        setNavvy({isActive: false})
-      } else {
-        setNavvy({isActive: true});
-      }
+  function setNavbarstatus() {
+    if (toggle) {
+      setNavvy(false);
+    } else {
+      setNavvy(true);
     }
+  }
 
-  return (
-    <div>
-      <div className="bg-gray-900 text-gray-400 max-w-full flex items-center justify-between">
-        <img className="h-8 m-4" src={logo} alt="Logo"></img>
-        <div className="font-plex font-bold px-4 md:text-xl lg:text-2xl">
-          A Blog About Learning To Code
+  if (toggle) {
+    return (
+      <div>
+        <div className="bg-gray-900 text-gray-400 max-w-full flex items-center justify-between">
+          <img className="h-8 m-4" src={logo} alt="Logo"></img>
+          <div className="font-plex font-bold px-4 md:text-xl lg:text-2xl">
+            A Blog About Learning To Code
+          </div>
+          <button className="mx-4" onClick={setNavbarstatus}>
+            <FontAwesomeIcon icon={faTimes} size="2x" />
+          </button>
         </div>
-        <button  
-          onClick={() => {setNavbarstatus()}}>
-          <FontAwesomeIcon icon={faBars} />
-        </button>
+        <ul className="flex flex-col items-end font-plex font-semibold p-4 bg-gray-900">
+          {navLinks.map((link, index) => (
+            <li className="p-2" key={index}>
+              <Link to={link.path}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
-        <div className={`${navvy.isActive}` ? `${"hide"}` : `${"font-plex block bg-gray-900 font-semibold p-4"}`}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit vel nam tempore quaerat ducimus, in voluptate beatae sunt deleniti numquam quasi non atque sequi a incidunt id officia voluptatem pariatur omnis ratione sit totam inventore quo magni. Rem eligendi harum officiis omnis est illo vel, consequatur consectetur ea facilis quaerat nostrum. Provident perspiciatis repudiandae quibusdam doloremque excepturi corrupti autem illo eos voluptate id molestiae, expedita et sapiente laborum recusandae maxime, illum fuga molestias libero enim culpa. Ut omnis sit officia velit blanditiis consectetur dolores in fugiat. Eveniet sunt debitis fugiat at facilis pariatur assumenda cumque possimus numquam qui suscipit illum optio, voluptas nobis maiores autem magni rem asperiores provident rerum commodi? Similique exercitationem in magnam molestiae labore provident, nam quas repellendus esse laudantium rem pariatur natus praesentium at adipisci, reiciendis beatae! Natus itaque accusantium alias nisi, libero laudantium qui dolore vel tempore nobis in, saepe possimus impedit maiores id voluptate ut perspiciatis atque? Commodi, officiis facere voluptas dignissimos error reiciendis odio hic doloremque? Quam est mollitia assumenda necessitatibus, magnam officiis architecto optio nam, illo eius in adipisci incidunt. Sapiente, deleniti incidunt non soluta esse illum. Ex, ut facere eaque fugit voluptas optio vero quam cumque molestias in laboriosam voluptate accusantium. Sit magni est nemo, aut deleniti fuga vel tempora. Natus culpa accusantium inventore nihil a ipsam nam quis tenetur magnam eius! Minus mollitia totam asperiores, vitae maiores incidunt deleniti delectus laborum! Nobis dolor nam molestias suscipit amet, odio sequi quia exercitationem assumenda ab placeat sunt cum quod repellat dolore dolores quaerat sit. Doloribus voluptas itaque eveniet ut unde qui minus officia facilis, accusamus modi aliquam alias distinctio a cum dignissimos dolorem repellat reiciendis, quo quia ab eius? Voluptates sed, voluptas ea recusandae tempore quia repellat eum quaerat vitae architecto veniam aliquam? Officiis temporibus hic voluptas velit quisquam quod, fugiat a doloribus repellat vero voluptatem natus quas, aliquid tempore et nisi molestias quae commodi officia minus neque eum corporis voluptatibus! Officiis itaque illo facilis optio maiores, non vero velit ipsum sapiente quod. Unde autem, voluptatem tempora rem cumque delectus illum explicabo nihil nisi rerum beatae, possimus perferendis. Id dolorem delectus nihil expedita accusamus eveniet corporis? Maiores saepe repudiandae iusto veniam possimus nemo aliquam deserunt, exercitationem itaque illum quo nihil corrupti nisi a, veritatis incidunt non eius! Alias consequuntur ut nihil velit amet accusantium rerum magni libero impedit? Aspernatur fuga quam nisi possimus? Itaque dolor molestiae enim molestias autem minima sed ipsa minus perspiciatis, similique iusto eveniet, fugiat animi dolores tempora error provident facere! Temporibus, fugit voluptatum error totam recusandae, eligendi quam nobis quae voluptas deleniti molestiae beatae et praesentium obcaecati non est, repellat distinctio ratione cum. Obcaecati nemo odio eius! Commodi voluptatibus accusantium vel. Pariatur possimus recusandae numquam commodi unde repellendus consequatur alias laboriosam ipsam adipisci deleniti fuga dolore eligendi rem quaerat molestiae quam architecto ea quasi iusto, nisi in! Voluptatum accusantium eveniet ducimus assumenda beatae similique omnis quibusdam rem illum optio aut velit ut facere, perspiciatis nemo dolores quo, deserunt obcaecati reprehenderit ullam eum? Repellendus eius vel soluta corporis quod totam non tempora aspernatur delectus, dolorem nobis sit aliquid quam. Distinctio voluptates, adipisci totam, eligendi, error consectetur illum maxime alias quis earum tempore at dolor. Odio reiciendis ut deleniti expedita iure atque, ex voluptatum doloremque? Est dolorum recusandae deleniti commodi dolorem numquam, cupiditate itaque non aut reiciendis sit et fugiat! Fugit vitae tempore commodi quis delectus modi provident eaque explicabo omnis sit quaerat iusto sint repellat libero placeat perspiciatis nemo, dicta magnam minus? Fugiat eos molestiae est eius obcaecati voluptatum tenetur architecto, dolore labore blanditiis ipsum similique aspernatur? Dolorum quo mollitia tempore error dolore, delectus eveniet iste laboriosam officiis qui.
+    );
+  }
+
+  if (!toggle) {
+    return (
+      <div>
+        <div className="bg-gray-900 text-gray-400 max-w-full flex items-center justify-between">
+          <img className="h-8 m-4" src={logo} alt="Logo"></img>
+          <div className="font-plex font-bold px-4 md:text-xl lg:text-2xl">
+            A Blog About Learning To Code
+          </div>
+          <button className="mx-4" onClick={setNavbarstatus}>
+            <FontAwesomeIcon icon={faMinus} size="2x" />
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default Navbar;
