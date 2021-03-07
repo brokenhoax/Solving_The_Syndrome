293d1032c42fa0be7c8dcf0010a435a247ed0582
brokenhoax <brokenhoax@outlook.com>
2021-03-07 13:58:18 -0600

    Added code snippet

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -1,96 +1,61 @@
 import React from "react";
 import "../../styles/scss/blog.scss";
 import "../../styles/app.css";
-import gridz from "../../assets/images/grid.jpg";
+import kungfoo from "../../assets/images/kung-foo.jpg";
 
 const Blog = () => {
   return (
     <section className="flex flex-col justify-center items-center mt-8">
-      <div className="flex flex-col justify-center mt-8 w-1/2">
+      <div className="flex flex-col justify-center w-1/2">
         <h1 className="text-4xl font-bold mt-24 mb-2">I Know Kung Foo</h1>
-        <h2 className="italic text-red-500">March 5th, 2021</h2>
-        <p className="text-2xl my-8">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
-          aperiam unde laboriosam cumque neque officiis esse fugit, sit totam
-          asperiores, animi molestias cum, nam voluptatibus nisi inventore eum
-          amet quibusdam. Necessitatibus saepe sit ea quo maiores inventore modi
-          magnam deleniti nostrum quae, id magni sunt mollitia pariatur nobis
-          delectus consectetur at sed? Dolor aliquid, suscipit eum illo tempore
-          fugiat veniam vitae accusamus quo? Sapiente laborum voluptatum
-          reiciendis, quod voluptatibus repellendus, maxime veniam quo neque
-          dolore laboriosam, iure autem deleniti dolor vitae. Ab nam hic
-          accusantium minima ipsam natus voluptas aperiam.
+        <h2 className="italic text-red-400">March 5th, 2021</h2>
+        <p className="text-2xl my-4 leading-10">
+          One of my favorite scenes from one of my favorite movies, The Matix,
+          is when the protagonist, Neo, is "learning" self-defense by having it
+          downloaded into his consciousness. After a ten-hour download binge,
+          Neo awakes, looks at his mentor, Morpheous, and says "I know Kung Fu".
+          Morpheous, without cracking a smile at the absurdity of it all, leans
+          in and says, "Show me".
         </p>
         <div className="flex justify-center items-center min-w-full">
           <img
-            src={gridz}
+            src={kungfoo}
             alt="Picture of Application"
-            class="flex justify-center items-center w-1/2 rounded-md"
+            className="flex justify-center items-center w-2/3 rounded-md"
           ></img>
         </div>
-        <p className="text-2xl my-8">
-          Aperiam reiciendis odit voluptate sit aspernatur ullam repellat iure,
-          modi necessitatibus odio blanditiis inventore repudiandae consequatur
-          magni qui vitae quas excepturi reprehenderit itaque corrupti dolorem
-          fuga quod sapiente! Ea vitae reiciendis dolor molestias. Nam
-          consequuntur ea illum adipisci, atque sunt obcaecati magni maiores
-          temporibus beatae, neque provident rem. Quod et totam adipisci sequi
-          aliquam suscipit veniam? Provident dignissimos dolor odio dolores
-          suscipit ducimus sit mollitia, repellendus repudiandae aut ipsam
-          voluptate temporibus ex, consectetur minus molestiae id? Voluptates
-          aliquam rerum temporibus eum hic, adipisci commodi suscipit eligendi
-          illum culpa dolores mollitia debitis dolor deleniti maiores quasi quo
-          sit a!
+        <p className="text-2xl my-4 leading-10">
+          The year was 1999 and I was a fifteen-year-old who spent most of his
+          spare time hooked to his computer playing Tom Clancy's Rainbow Six and
+          blasting The Chemical Brothers. The thought of being able to learn,
+          really, anything without having to put in the sweat equity blew my
+          mind. Hook me up to that digital teat and I'd go bananas.
         </p>
-        <p className="text-2xl my-8">
-          Consequature impedit molestiae explicabo itaque, dolorum modi
-          voluptatem saepe voluptas accusantium voluptatibus obcaecati mollitia
-          aliquam ratione dignissimos fugiat. Ut, voluptatem molestiae corrupti
-          mollitia, nihil cupiditate porro fugit eaque sunt id iure nobis
-          dignissimos ipsum unde nesciunt dolorem fuga placeat, asperiores
-          assumenda iste aliquid minus rem optio dolores. Nulla, quaerat veniam.
-          Saepe, dicta vitae, vero molestiae, porro animi est quidem unde
-          delectus officiis illum? Repudiandae, sequi voluptatum atque
-          cupiditate dolorum porro, molestiae, similique dolores obcaecati vero
-          in adipisci minus non nobis fuga ipsum libero totam perferendis odio
-          esse quo debitis sapiente! Iste sint iure sequi nostrum cumque tenetur
-          accusamus blanditiis. Dignissimos quaerat consectetur minima qui iure,
-          unde consequuntur rem laboriosam numquam tempore quis recusandae iste
-          ipsam nam at suscipit laborum maiores obcaecati neque deserunt
-          voluptates cupiditate tempora accusamus modi. Neque omnis quos, cum
-          eius ipsum nulla perferendis eos beatae corporis vitae molestias ad
-          voluptates consequatur quaerat? Quae fugiat, dolorum odit quod vero
-          hic veniam molestias officia numquam enim dolores quam a ad, et cumque
-          eveniet excepturi delectus autem voluptatem! Rem quisquam ipsa
-          perspiciatis aut autem, nihil minima harum quasi ipsam atque maxime
-          asperiores a provident quo iste est cumque modi omnis fugiat
-          consequatur aspernatur ducimus ut voluptas numquam. Consequatur sit,
-          tempore perspiciatis blanditiis, amet in asperiores sapiente cumque
-          labore maxime sunt vitae nobis perferendis veritatis nulla. Beatae,
-          quasi corporis! Blanditiis facere, minima consequatur ut tempora
-          officia voluptatum a corporis debitis molestias doloremque placeat
-          repudiandae dolorem, deleniti hic obcaecati culpa consectetur dolor
-          nemo temporibus ducimus dolores? Et molestias dolorum natus! Fuga quas
-          aspernatur dolores ea sequi tempora consectetur aliquam blanditiis
-          fugiat quo dolore esse, facere obcaecati at nihil odio laudantium
-          molestias numquam. Corporis possimus quis, eveniet, nisi quod
-          exercitationem a beatae obcaecati, consectetur distinctio aliquid
-          ducimus. Corrupti iusto commodi repellendus molestiae sequi recusandae
-          est quos. Blanditiis reiciendis quibusdam quidem neque perspiciatis
-          unde molestias assumenda provident sint. Quisquam maiores asperiores
-          inventore ea fugiat aspernatur fuga architecto, ad voluptates impedit
-          reprehenderit dolore nemo! Nisi, sequi consequuntur nesciunt dicta
-          consequatur deserunt quisquam quibusdam voluptas ipsa? Labore
-          architecto hic, dolore cum nesciunt excepturi. Possimus labore iure
-          qui fuga dolore provident esse, alias, atque perferendis id quam
-          beatae quas asperiores ea odit doloribus, suscipit cum odio laudantium
-          reiciendis ab totam. Cupiditate nisi ex architecto veritatis nam quod
-          eos, maiores fugiat vitae ipsum delectus distinctio blanditiis dolorem
-          rem consequatur. Quod necessitatibus expedita labore ipsa culpa, esse
-          ab exercitationem neque repellendus sed, soluta quaerat, consequatur
-          ut architecto magnam consequuntur veniam. Necessitatibus eveniet
-          adipisci enim laudantium voluptatum porro dicta nisi ducimus. Fugit.
+        <p className="text-2xl my-4 leading-10">
+          Fast-forward to 2021 and,{" "}
+          <a href="https://www.w3schools.com" class="text-red-400">
+            thanks to visionaries like Elon Musk
+          </a>
+          , we're getting closer to making that a reality. Unfortunately, at 36,
+          the once alluring prospect of being able to become a master of
+          anything with the click of a button has now faded. Six job and two
+          career paths later, I realize how difficult it is to master anything
+          and the thought of somebody being able to just upload some code to
+          achieve the same just makes me want to puke.
         </p>
+        <p className="text-2xl my-4">How I benefit from this blog.</p>
+        <pre className="text-sm self-center prettyprint bg-gray-900 w-2/3 leading-8 rounded-xl lang-js overflow-auto ">
+          {`
+    function test() {
+        let i = 0;
+        for (i; i > 42; i++) {
+          console.log("Mad skills!")
+        }
+    }
+              `}
+        </pre>
+        <p className="text-2xl my-8">Who can benefit from this blog.</p>
+        <p className="text-2xl my-8">Where I plan on taking this blog</p>
       </div>
     </section>
   );
