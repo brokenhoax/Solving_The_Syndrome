f66997f306bf6afde40bec6e56ecc256b036f792
brokenhoax <brokenhoax@outlook.com>
2021-03-10 00:29:01 -0600

    Blog Post 1 Complete

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -12,57 +12,92 @@ const Blog = () => {
     document.body.appendChild(script);
   }, []);
   return (
-    <section className="flex flex-col justify-center items-center mt-16">
-      <div className="flex flex-col justify-center w-2/5">
-        <h1 className="text-4xl font-bold mt-24 mb-2">I Know Kung Foo</h1>
+    <section className="flex flex-col items-center mt-16">
+      <div className="w-1/3">
+        <h1 className="self-center text-4xl font-bold mt-24 mb-2">
+          I Know Kung Foo
+        </h1>
         <h2 className="italic text-red-400">March 5th, 2021</h2>
-        <p className="text-2xl my-4 leading-10 mt-12">
+        <p className="text-xl my-8 leading-10 mt-12">
           One of my favorite scenes from one of my favorite movies, The Matix,
-          is when the protagonist, Neo, is learning self-defense by having it
-          downloaded into his consciousness. After a ten-hour download binge,
-          Neo awakes, looks at his mentor, Morpheous, and says "I know Kung Fu".
-          Morpheous, without cracking a smile at the absurdity of it all, leans
-          in and says, "Show me".
+          is when the protagonist, Neo, is learning Kung Fu by downloading it to
+          his consciousness. After a ten-hour download binge, Neo awakes, looks
+          at his mentor, Morpheous, and says "I know Kung Fu". Morpheous,
+          without cracking a smile, leans in and says, "Show me".
         </p>
-        <div className="flex justify-center items-center min-w-full">
+        <div className="flex flex-col items-center">
           <img
             src={kungfoo}
             alt="Application"
-            className="flex justify-center items-center w-2/3 rounded-md"
+            className="w-3/5 rounded-lg"
           ></img>
         </div>
-        <p className="text-2xl my-4 leading-10">
+        <p className="text-xl my-8 leading-10">
           The year was 1999 and I was a fifteen-year-old who spent most of his
-          spare time hooked to his computer playing Tom Clancy's Rainbow Six and
-          blasting The Chemical Brothers. The thought of being able to learn,
-          really, anything without having to put in the sweat equity blew my
-          mind. Hook me up to that digital teat and I'd go bananas.
+          spare time hooked to his computer terrorizing chat rooms and trying to
+          squeeze every last drip of RAM out of my PC so I could play Tom
+          Clancy's Rainbow Six. The thought of being able to learn anything
+          without having to put in the sweat equity blew my mind. Hook me up to
+          that digital teat and I'd go bananas.
         </p>
-        <p className="text-2xl my-4 leading-10">
-          Fast-forward to 2021 and,{" "}
+        <p className="text-xl my-8 leading-10">
+          Fast-forward to 2021, and
           <a href="https://neuralink.com/" class="text-red-400">
-            thanks to visionaries like Elon Musk
-          </a>
-          , we're getting closer to making that a reality. Unfortunately, at 36,
-          the once alluring prospect of being able to become a master of
-          anything with the click of a button has now faded. Six job and two
-          career paths later, I realize how difficult it is to master anything
-          and the thought of somebody being able to just upload some code to
-          achieve the same just makes me want to puke.
+            {" "}
+            we're getting closer to making that a reality.
+          </a>{" "}
+          I never would have thought that, by age 36, the idea of somebody being
+          able to just download some code to achieve a life's work would make me
+          want to puke. Think about a world where anybody could be whatever they
+          wanted to be without having to actually earn their way. Who would we
+          look up to anymore? What would we strive for and why would we even
+          try? In the Matrix, what makes Neo superior to his enemy is his
+          anomoly or, as others might say, his God-given ability. He's the oNe.
+          What does that make everyone else?
         </p>
-        <p className="text-2xl my-4">How I benefit from this blog.</p>
-        <pre className="text-lg self-center prettyprint text-red-400 bg-gray-900 w-2/3 leading-10 rounded-xl overflow-auto">
-          {`
-    function test() {
-        let i = 0;
-        for (i; i > 42; i++) {
-          console.log("Mad skills!")
+        <p className="text-xl my-8 leading-10">
+          Thankfully, like Neo, most of us are given a choice in life. Take the
+          blue pill, and play it safe, or take the red pill, and step into the
+          unknown. There's nothing unknown about a world where anyone can be
+          anything. Every challenge, every adventure would be as thrilling as
+          tic-tac-toe.
+        </p>
+        <div className="flex flex-col items-center">
+          <pre className="w-3/5 text-sm prettyprint text-red-400 bg-gray-900 leading-10 rounded-lg overflow-auto">
+            {`
+    function life() {
+        let chance = Math.floor(Math.random() * 10) + 1;
+        if ( chance > 5 ) {
+          return console.log ("You live!") 
+        } else {
+          return console.log ("You die!")
         }
     }
               `}
-        </pre>
-        <p className="text-2xl my-8">Who can benefit from this blog.</p>
-        <p className="text-2xl my-8">Where I plan on taking this blog</p>
+          </pre>
+        </div>
+        <p className="text-xl my-8 leading-10">
+          Herein lies the philosophy behind "Solving the Syndrome". This is my
+          red pill. This is a place where I will venture into the unknown. Along
+          the way, I hope to learn and grow and overcome my biggest fear. My
+          biggest fear is not living up to my own potential. I'm okay with
+          knowing my potential is limited, but I'm not okay with never finding
+          my limit. I think that's where I'm fortunate. I could be comfortable
+          but I wouldn't be well. I could be happy but I wouldn't be satisfied.
+        </p>
+        <p className="text-xl my-8 leading-10">
+          This blog is dual purpose. Over the past year, I've learned to code
+          and have been fortunate enough to land my first developer job with a
+          start-up out of Chicago. In a way, Cypher was right when he said
+          "Ignorance is bliss". It seems that the more I learn, the more I
+          realize how little I know. This blog will serve as a reminder to me of
+          how far I've come and it is meant to serve others as well. There's
+          limitless resources available that can help you learn to code;
+          however, I have yet to find one that chronicles how and what one
+          learns to code as they learn. In time, I hope to look back on my
+          earlier posts and admire my naivety and enthusiasm. I hope you do,
+          too.
+        </p>
       </div>
     </section>
   );
